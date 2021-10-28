4bd59c379c48f01d89423549ee6fd3a0d50cad2e
umar-hasan <umarhasan16@gmail.com>
2021-10-27 23:49:28 -0400

    Bug fix

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -13,15 +13,10 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
-<<<<<<< Updated upstream
-        ssl: true,
-        rejectUnauthorized: false
-=======
         ssl: {
             require: true,
             rejectUnauthorized: false
         }
->>>>>>> Stashed changes
     },
     logging: false
 })
